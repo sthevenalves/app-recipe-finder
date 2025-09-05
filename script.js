83e8f941900147eb25d3836ac4b89c9a378ffec1
Stheven <sthevenalvess@gmail.com>
2025-09-05 17:10:21 -0300

    feat(js): buscar informacoes da receita

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//www.themealdb.com/api/json/v1/1/filter.php?i=
+//www.themealdb.com/api/json/v1/1/filter.php?i=name
+//www.themealdb.com/api/json/v1/1/lookup.php?i=id
 const input = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
 const recipeList = document.querySelector('.recipe-list')
@@ -21,11 +22,28 @@ async function searchRecipes(ingredient) {
 
 function showRecipes(recipes) {
     recipeList.innerHTML = recipes.map(
-        element => `
-        <div class="recipe-card">
+        (element) => `
+        <div class="recipe-card" onClick="getRecipesDetails(${element.idMeal})">
         <img src="${element.strMealThumb}" alt="receita-foto">
         <h3>${element.strMeal}</h3>
         </div>
         `
     ).join('');
+}
+
+async function getRecipesDetails(id) {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const data = await response.json();
+    const recipe = data.meals[0];
+    
+    let ingredients = '';
+
+    for (let i = 1; i <= 20; i++) {
+        if (recipe[`strIngredient${i}`]) {
+            ingredients += `<li>${recipe[`strIngredient${i}`]} - 
+                            ${recipe[`strMeasure${i}`]}</li>`;
+        } else {
+            break;
+        }
+    }
 }
