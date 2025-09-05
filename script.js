05f1fe79f58e2adbb51d2c371513adc571eadd7a
Stheven <sthevenalvess@gmail.com>
2025-09-05 17:39:54 -0300

    css: informacoes e imagens das receitas clicadas

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const input = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
 const recipeList = document.querySelector('.recipe-list')
+const recipeDetails = document.querySelector('.recipe-details');
 
 
 form.addEventListener('submit', function (event) {
@@ -35,7 +36,7 @@ async function getRecipesDetails(id) {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
     const data = await response.json();
     const recipe = data.meals[0];
-    
+
     let ingredients = '';
 
     for (let i = 1; i <= 20; i++) {
@@ -46,4 +47,17 @@ async function getRecipesDetails(id) {
             break;
         }
     }
+
+    recipeDetails.innerHTML = `
+        <h2>${recipe.strMeal}</h2>
+        <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}" class="recipe-img">
+        <h3>Categoria: ${recipe.strCategory}</h3>
+        <h3>Origem: ${recipe.strArea}</h3>
+        <h3>Ingredientes:</h3>
+        <ul>${ingredients}</ul>
+        <h3>Instruções:</h3>
+        <p>${recipe.strInstructions}</p>
+        <p>Tags: ${recipe.strTags}</p>
+        <p>Video: <a href="${recipe.strYoutube}" target="_blank">Assista no Youtube</a></p>
+    `;
 }
